675757a6c9fbcbdc941a365100a67900b22ceec7
Shahzalal <your-github-email@gmail.com>
2025-09-21 11:12:38 +0600

    Add splash screen ui

--- lib/core/routes/route_configs.dart
+++ lib/core/routes/route_configs.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:mussweg/core/routes/route_names.dart';
 import 'package:mussweg/views/home/screen/home_screen.dart';
 import 'package:mussweg/views/parent_screen/screen/parent_screen.dart';
+import 'package:mussweg/views/splash/splash_screen.dart';
 
 
 class AppRoutes {
   static final Map<String, WidgetBuilder> routes = {
+    RouteNames.splashScreen: (context) => const SplashScreen(),
     RouteNames.parentScreen: (context) => const ParentScreen(),
   };
 }
